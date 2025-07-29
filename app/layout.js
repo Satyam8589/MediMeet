df34095a193ea230779ed7e0a00a7186bfa312ad
Satyam8589 <satyamkumarsingh9898@gmail.com>
2025-07-29 22:17:52 +0530

    doctore and patience layout is now working

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
             {/* Header Section */}
             <Header />
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
 
             {/* Footer Section */}
             <footer className="bg-muted/50 py-12">
